00601b3e8edadba2350fd4c452a5005f3424830e
Aislan Santos <aislan_85@yahoo.com.br>
2022-01-14 11:27:36 -0300

    terminado o cadastro inicial de clinte com edição e exclusão

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -2,9 +2,18 @@ from django.urls import path
 from . import views
 import cadastros
 
+
 urlpatterns = [
     path('', views.home, name='home'),
+
+]
+
+
+# urls clientes
+urlpatterns += [
     path('clientes/', views.list_clientes, name='list_clientes'),
-    path('clientes/novo', views.new_cliente, name='new_cliente')
-    
+    path('cliente/novo', views.new_cliente, name='new_cliente'),
+    path('cliente/deletar/<int:pk>/', views.delete_cliente, name="delete_cliente"),
+    path('cliente/<int:pk>', views.detail_cliente, name='detail_cliente'),
+    path('cliente/<int:pk>/edit', views.edit_cliente, name='edit_cliente'),
 ]
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,24 +1,54 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cliente
 from .forms import ClienteForm
 
 # Create your views here.
 
 # redicionamento de paginas
+
+
 def home(request):
     return render(request, 'cadastros/index.html')
+
 
 def list_clientes(request):
     clientes = Cliente.objects.all()
     return render(request, 'cadastros/clientes.html', {'clientes': clientes})
 
+def detail_cliente(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    return render(request, 'cadastros/clientes_cadastro.html', {'cliente': cliente})
+
+
 def new_cliente(request):
-    if request.method == "POST":
-        form = ClienteForm()
+    if request.method == 'POST':
+        form = ClienteForm(request.POST)
         if form.is_valid():
-            cliente = form.save(commit=False)
-            cliente.save()
+            form.save()
+            clientes = Cliente.objects.all()
+            return render(request, 'cadastros/clientes.html', {'clientes': clientes})
     else:
         form = ClienteForm()
+        return render(request, 'cadastros/clientes_cadastro.html', {'form': form})
     clientes = Cliente.objects.all()
-    return render(request, 'cadastros/index.html')
+    return render(request, 'cadastros/clientes.html', {'clientes': clientes})
+
+
+def edit_cliente(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    if request.method == "POST":
+        form = ClienteForm(request.POST, instance=cliente)
+        if form.is_valid():
+            form.save()
+            return redirect('list_clientes')
+    else:
+        form = ClienteForm(instance=cliente)
+        return render(request, 'cadastros/clientes_cadastro.html', {'form': form})
+    clientes = Cliente.objects.all()
+    return render(request, 'cadastros/clientes.html', {'clientes': clientes})
+
+
+def delete_cliente(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    cliente.delete()
+    return redirect('list_clientes')
